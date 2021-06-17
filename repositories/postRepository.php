54b33cd4f30fb594eb226d497ff4f516c3003b43
chaotic98 <61518809+chaotic98@users.noreply.github.com>
2021-06-17 05:57:18 -0700

    select all posts

--- repositories/postRepository.php
+++ repositories/postRepository.php
@@ -1,13 +1,12 @@
 <?php
-require ("../db.php");
-
+require_once __DIR__."/../db.php";
+require_once __DIR__."/../index.php";
 class PostRepository {
     private $conn;
     public function __construct()
     {
         $this->conn = DB::getInstance();
     }
-
     public function addPost($post){
         try {
             $sql = "INSERT INTO posts (title, body,author,image)VALUES ('$post->title' , '$post->body' , '$post->author', '$post->image')";
@@ -18,4 +17,28 @@ class PostRepository {
             echo $e->getMessage();
     }
     }
+
+    public function getAllPosts(){
+        try {
+            $sql="select * from posts ";
+            $statment=$this->conn->prepare($sql);
+            $statment->execute();
+            $result = $statment->setFetchMode(PDO::FETCH_OBJ);
+
+        }
+        catch (PDOException $e){
+            $e->getMessage();
+        }
+    } public function getPostsById($id){
+        try {
+            $sql="select * from posts where id=$id";
+            $statment=$this->conn->prepare($sql);
+            $statment->execute();
+            $result = $statment->setFetchMode(PDO::FETCH_OBJ);
+        }
+        catch (PDOException $e){
+            $e->getMessage();
+        }
+    }
+
 }
