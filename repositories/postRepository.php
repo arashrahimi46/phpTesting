46fc9de54d37919b0a7244c5fb2cfb013a747e93
armin <you@example.com>
2021-06-17 03:46:32 -0700

    admin form complete

--- repositories/postRepository.php
+++ repositories/postRepository.php
@@ -1,1 +1,14 @@
 <?php
+require ("../db.php");
+
+class PostRepository {
+    private $conn;
+    public function __construct()
+    {
+        $this->conn = DB::getInstance();
+    }
+
+    public function addPost($request){
+        $sql = "INSERT INTO posts (title, body,author,image)VALUES ($request)";
+    }
+}
